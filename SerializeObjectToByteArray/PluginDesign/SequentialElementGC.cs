08831dd524aa9d0d2b5491127b646a3632743631
Jose A. Fernandez <fernandezja@gmail.com>
2018-02-26 08:17:14 -0300

    Example > Serialize object to byte array

---
+++ SerializeObjectToByteArray/PluginDesign/SequentialElementGC.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Xml.Serialization;
+
+namespace PluginDesign.ScreenElements.SequentialElement
+{
+
+    [Serializable]
+    [XmlRoot("elementoSecuencial")]
+    public class SequentialElementGC
+    {
+        [XmlAttribute("id")]
+        public string IdElement { get; set; }
+
+        [XmlAttribute("esEstimuloCritico")]
+        public bool EstimuloCritico { get; set; }
+
+        [XmlAttribute("idView")]
+        public string Id { get; set; }
+
+        public SequentialElementGC()
+        {
+        }
+
+       
+    }
+}
