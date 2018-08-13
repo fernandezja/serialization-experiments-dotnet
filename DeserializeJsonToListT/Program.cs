3f5b2eb76f9ef6ac9a4450488cd4157fab9b471d
Jose A. Fernandez <fernandezja@gmail.com>
2018-08-12 21:52:32 -0300

    Set JsonSerializerSettings for date DateFormatHandling MicrosoftDateFormat

--- DeserializeJsonToListT/Program.cs
+++ DeserializeJsonToListT/Program.cs
@@ -13,16 +13,28 @@ namespace DeserializeJsonToListT
 
             var path = Path.Combine(Environment.CurrentDirectory,  "data.json");
 
-            var jsonString = File.ReadAllText(@"data.json");
-            var clientes = JsonConvert.DeserializeObject<List<Cliente>>(jsonString);
-            Console.WriteLine($"Clientes cantidad: {clientes.Count}");
+            var serializerSettings = new JsonSerializerSettings(){
+                DateFormatHandling = DateFormatHandling.MicrosoftDateFormat
+            };
 
             if (File.Exists(path))
             {
                 
                 var jsonString = File.ReadAllText(@"data.json");
-                var clientes = JsonConvert.DeserializeObject<List<Cliente>>(jsonString);
+                var clientes = JsonConvert
+                                .DeserializeObject<List<Cliente>>(jsonString, 
+                                                                  serializerSettings);
                 Console.WriteLine($"Clientes cantidad: {clientes.Count}");
+
+                foreach (var item in clientes)
+                {
+                    Console.WriteLine($"    |_ Nombre: {item.Nombre}");
+                    Console.WriteLine($"    |_ FechaCreacion: {item.FechaCreacion}");
+                    Console.WriteLine($"    |_ FechaModificacion: {item.FechaModificacion}");
+                    Console.WriteLine($"    |_ FechaModificacionUtc: {item.FechaModificacionUtc}");
+
+                    Console.WriteLine($" --------------------------------------------");
+                }
             }
             else
             {
