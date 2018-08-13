61ce523f41bf3c7a76b175e16560c9d94abf85b2
Jose A. Fernandez <fernandezja@gmail.com>
2018-08-12 21:21:53 -0300

    Example Deserialize Json To List<T>

---
+++ DeserializeJsonToListT/Cliente.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DeserializeJsonToListT
+{
+    public class Cliente
+    {
+        public int Numero { get; set; }
+        public string Id { get; set; }
+        public string Nombre { get; set; }
+        public int Edad { get; set; }
+        public string Telefono { get; set; }
+        public string Mail { get; set; }
+        public decimal Saldo { get; set; }
+        public DateTime FechaCreacion { get; set; }
+        public DateTime FechaCreacionUtc { get; set; }
+        public DateTime FechaModificacion { get; set; }
+        public DateTime FechaModificacionUtc { get; set; }
+        public int Proceso { get; set; }
+        public string Usuario { get; set; }
+        public string Estado { get; set; }
+    }
+}
