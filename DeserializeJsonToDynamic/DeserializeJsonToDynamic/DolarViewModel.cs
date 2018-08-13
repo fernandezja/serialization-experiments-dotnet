7fdc60d01c1ad33041449aa5dd00f27a047dd37e
Jose A. Fernandez <fernandezja@gmail.com>
2018-08-12 21:23:23 -0300

    Deserialization JSON to Dynamic

---
+++ DeserializeJsonToDynamic/DeserializeJsonToDynamic/DolarViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeserializeJsonToDynamic
+{
+    public class DolarViewModel
+    {
+        public List<ListaValoresDolarViewModel> Dolares { get; set; }
+    }
+
+    public class ListaValoresDolarViewModel
+    {
+        public string Valor { get; set; }
+        public string Fecha { get; set; }
+    }
+
+}
