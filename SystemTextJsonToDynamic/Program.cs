0acc6491abe75ad260597387956e4abdc9854f9d
fernandezja <fernandezja@gmail.com>
2021-01-30 11:58:37 -0300

    SystemTextJsonToDynamic > System.Text.Json with with anonymous type

---
+++ SystemTextJsonToDynamic/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Dynamic;
+using System.Text.Json;
+
+namespace SystemTextJsonToDynamic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var jsonData = "{\"Title\":\"Star Wars: Episode IV - A New Hope\", \"Year\":1977}";
+
+            //with anonymous type 
+            var movie = new
+            {
+                Title = "",
+                Year= 0
+            };
+
+
+            var options = new JsonSerializerOptions { 
+                PropertyNameCaseInsensitive = true, 
+                IncludeFields = true };
+
+            var object1 = JsonSerializer.Deserialize(jsonData, movie.GetType());
+
+            Console.Write($"Data Json: {object1}");
+
+            Console.ReadKey();
+        }
+    }
+}
