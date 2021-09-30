b33c26b0a16f9f83808e3a4fa7ad2d018eabcac1
fernandezja <fernandezja@gmail.com>
2021-09-29 21:05:57 -0300

    SystemTextJson > Console. Read config

---
+++ SystemTextJson/SystemTextJsonConsole/CurrentConfiguration.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SystemTextJsonConsole
+{
+    public class CurrentConfiguration : ICurrentConfiguration
+    {
+        public string ApiUrl { get; set; }
+        public string ClientId { get; set; }
+        public string ClientSecret { get; set; }
+        public string Scope { get; set; }
+        public string GoogleAndroidClientId { get; set; }
+        public string GoogleiOSClientId { get; set; }
+        public string FacebookClientId { get; set; }
+        public string BranchOfficesMapUrl { get; set; }
+        public string Web { get; set; }
+        public string Facebook { get; set; }
+        public string Twitter { get; set; }
+        public string Instagram { get; set; }
+        public string Whatsapp { get; set; }
+    }
+}
---
+++ SystemTextJson/SystemTextJsonConsole/ICurrentConfiguration.cs
@@ -0,0 +1,19 @@
+﻿namespace SystemTextJsonConsole
+{
+    public interface ICurrentConfiguration
+    {
+        string ApiUrl { get; set; }
+        string BranchOfficesMapUrl { get; set; }
+        string ClientId { get; set; }
+        string ClientSecret { get; set; }
+        string Facebook { get; set; }
+        string FacebookClientId { get; set; }
+        string GoogleAndroidClientId { get; set; }
+        string GoogleiOSClientId { get; set; }
+        string Instagram { get; set; }
+        string Scope { get; set; }
+        string Twitter { get; set; }
+        string Web { get; set; }
+        string Whatsapp { get; set; }
+    }
+}
